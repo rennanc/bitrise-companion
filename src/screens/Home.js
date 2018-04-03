a7171c7b140c52e794830ae0e5802532f5cc81e3
rennanc <rennanchagas@outlook.com>
2018-04-02 22:13:39 -0300

    added react-native-vector-icons; added logout button;

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,10 +6,13 @@ import {
   View,
   FlatList,
   Dimensions,
-  AsyncStorage
+  AsyncStorage,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 import Aplicacao from '../components/Aplicacao'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Home extends Component {
 
@@ -17,25 +20,36 @@ export default class Home extends Component {
     super();
     this.state = {
       builds: [],
-      token: ''
+      token: '',
+      user: ''
     }
   }
 
   logout() {
     AsyncStorage.clear();
     this.props.navigator.resetTo({
-      screen: {
-        screen: 'Login',
-        title: 'Login'
+      screen: 'Login',
+      title: 'Login',
+      navigatorStyle: {
+        navBarHidden: true
       }
-    })
+    });
   }
 
   componentDidMount() {
+    //load user
+    AsyncStorage.getItem('user')
+      .then(user =>{
+        if(user) {
+          this.setState({user : user})
+        }
+      });
+
+    //load token
     AsyncStorage.getItem('token')
       .then(token => {
         if (token) {
-          this.setState({ token: token })
+          this.setState({token})
         }
       })
       .then(() => {
@@ -57,16 +71,17 @@ export default class Home extends Component {
           )
 
       })
-
-
-
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.cabecalho}>
+          <Image source={{uri: this.state.user.avatar_url}} style={styles.foto}/>
           <Text style={styles.titulo}>Bitrise Companion</Text>
+          <TouchableOpacity onPressIn={() => this.logout()} style={styles.button_exit}>
+            <Icon size={30} name="exit-to-app" color="#fff"/>
+          </TouchableOpacity>
         </View>
         <FlatList style={styles.lista}
           keyExtractor={item => item.slug}
@@ -90,13 +105,18 @@ const styles = StyleSheet.create({
   cabecalho: {
     height: 50,
     backgroundColor: '#3aa792',
+    flexDirection: 'row'
+  },
+  button_exit:{
+    marginRight: 5,
+    justifyContent: 'center',
   },
   titulo: {
+    flex:1,
     color: 'white',
     fontSize: 19,
     fontWeight: 'bold',
-    marginTop: 10,
-    textAlignVertical: 'bottom',
+    textAlignVertical: 'center',
     textAlign: 'center'
   }
 });
